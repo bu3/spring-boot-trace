3958f9b36297a0c7dedb9ca2005c30c97ad54b97
Fabio Mangione <fmangione@salesforce.com>
2024-11-05 15:58:45 +0000

    fix tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpMethod.TRACE;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalManagementPort;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -34,7 +36,7 @@ class DemoApplicationTests {
      */
     @Test
     public void testTraceDisabledApi() throws URISyntaxException {
-        verifyTraceFails(this.apiPort);
+        verifyTraceFails(this.apiPort, METHOD_NOT_ALLOWED);
     }
 
     /**
@@ -42,19 +44,19 @@ class DemoApplicationTests {
      */
     @Test
     public void testTraceDisabledManagement() throws URISyntaxException {
-        verifyTraceFails(this.managementPort);
+        verifyTraceFails(this.managementPort, FORBIDDEN);
     }
 
-    private void verifyTraceFails(final int port) throws URISyntaxException {
+    private void verifyTraceFails(final int port, HttpStatusCode code) throws URISyntaxException {
         final String url = "http://localhost:" + port;
         RestTemplate restTemplate = new RestTemplate();
         try {
             RequestEntity<Void> requestEntity = new RequestEntity<>(TRACE, new URI(url));
             ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
-            assertThat(response.getStatusCode(), Matchers.equalTo(METHOD_NOT_ALLOWED));
+            assertThat(response.getStatusCode(), equalTo(code));
         }
         catch (org.springframework.web.client.HttpClientErrorException e) {
-            assertThat(e.getStatusCode(), Matchers.equalTo(METHOD_NOT_ALLOWED));
+            assertThat(e.getStatusCode(), equalTo(code));
             return;
         }
         catch (Exception e) {
